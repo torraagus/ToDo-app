bb19094fbaa86a7725e91a006b22e3bf720431ea
Torra Agustin <torra.agustinignacio@gmail.com>
2020-12-04 22:03:53 -0300

    Add styled components

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,24 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { ToDo, DeleteBtn, UpdateBtn, Description, ActionBtns } from "./todoList.styles";
 
 const TodoList = ({ todos, onDeleteToDo, onUpdateToDo }) => {
-	return (
-		<div>
-			<h4>List</h4>
-			{todos.map((todo, index) => (
-				<Fragment key={index}>
-					<p>{todo}</p>
-					<button onClick={() => onDeleteToDo(todo)}>Delete</button>
-					<button onClick={() => onUpdateToDo(todo)}>Update</button>
-				</Fragment>
-			))}
-			{todos.length === 0 && <p>You don't have todos</p>}
-		</div>
-	);
+  return (
+    <div>
+      <h4>List</h4>
+      {todos.map((todo, index) => (
+        <ToDo key={index}>
+          <Description>{todo}</Description>
+          <ActionBtns>
+            <DeleteBtn onClick={() => onDeleteToDo(todo)}>Delete</DeleteBtn>
+            <UpdateBtn onClick={() => onUpdateToDo(todo)}>Update</UpdateBtn>
+          </ActionBtns>
+        </ToDo>
+      ))}
+      {todos.length === 0 && <p>You don't have todos</p>}
+    </div>
+  );
 };
 
 TodoList.propTypes = {
-	todos: PropTypes.arrayOf(PropTypes.string),
+  todos: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default TodoList;
